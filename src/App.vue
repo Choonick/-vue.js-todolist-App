1486ba75a575848feadbbfa17e11e776a128b9de
Choonick <wecube.1@gmail.com>
2018-04-02 17:50:30 +0900

    todo list app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>To do List App</h1>
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  components: {
+    TodoList
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <BaseInputText 
+        v-model="newTodoText" 
+        @keydown.enter="addTodo"/>
+        <ul v-if="todos.length">
+            <TodoListItem 
+            v-for="todo in todos" 
+            :key="todo.id" 
+            :todo="todo" 
+            @remove="removeTodo"/>
+        </ul>
+        <p v-else>
+            해야할 일을 추가해 주세요.
+        </p>
+    </div>
+</template>
+
+<script>
+import BaseInputText from "./BaseInputText.vue";
+import TodoListItem from "./TodoListItem.vue";
+
+let nextTodoId = 1
+
+export default {
+  components: {
+    BaseInputText,
+    TodoListItem
+  },
+  
+  data() {
+    return {
+      newTodoText: '',
+        todos: [
+                {
+                    id: nextTodoId++,
+                    text: 'qqq'
+                },
+                {
+                    id: nextTodoId++,
+                    text: "www"
+                },
+                {
+                    id: nextTodoId++,
+                    text: "zxc"
+                }
+        ]
+    }
+  },
+  methods: {
+    addTodo() {
+    
+      const trimmedText = this.newTodoText.trim();
+      console.log(this.newTodoText);
+      if (trimmedText) {
+        this.todos.push({
+          id: nextTodoId++,
+          text: trimmedText
+        });
+        this.newTodoText = '';
+      }
+    },
+    removeTodo(idToRemove) {
+      this.todos = this.todos.filter(todo => {
+        return todo.id !== idToRemove;
+      })
+    }
+  }
+};
+</script>
+
